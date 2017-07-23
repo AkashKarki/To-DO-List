5da19079d3f5bd8049f6c4febea4a078032409bc
akash <akashkarki10@gmail.com>
2017-07-23 18:11:47 +0530

    Todo's List is being Displayed

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 var TodoApp = angular.module('ngTodo',[]);
 
 TodoApp.controller('mainController',function($scope) {
-
+        $scope.TodoList=[{'Title':'Navy form',
+                           'Todo':'Submit the Navy form till 31th'},
+                           {'Title':'foodieApp',
+                           'Todo':'Submit the angular App "FoodieAPP" till 27th'},
+                           {'Title':'Electricity Bill',
+                           'Todo':'pay 1500rs Electricity bill till 1 Aug'},
+                           {'Title':'CodeVita',
+                           'Todo':'Take part in CodeVita on Monday'},
+                           {'Title':'Party',
+                           'Todo':"Attend Ankti's Party 7:30pm"}];
 })
