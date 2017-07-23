891a4dc1e768e41f76f2975e1d05c9f42390c667
akash <akashkarki10@gmail.com>
2017-07-23 22:21:28 +0530

    user can add TODO

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var TodoApp = angular.module('ngTodo',[]);
 
-TodoApp.controller('mainController',function($scope) {
-        $scope.TodoList=[{'Title':'Navy form',
+TodoApp.controller('mainController',function($scope) { 
+    //localStorage.setItem("TodoLists",null);
+    var TodoLists=JSON.parse(localStorage.getItem("TodoLists"));
+        if(TodoLists==null)
+        {
+            $scope.TodoList=[{'Title':'Navy form',
                            'Todo':'Submit the Navy form till 31th'},
                            {'Title':'foodieApp',
                            'Todo':'Submit the angular App "FoodieAPP" till 27th'},
@@ -10,5 +14,53 @@ TodoApp.controller('mainController',function($scope) {
                            {'Title':'CodeVita',
                            'Todo':'Take part in CodeVita on Monday'},
                            {'Title':'Party',
-                           'Todo':"Attend Ankti's Party 7:30pm"}];
+                           'Todo':"Attend Ankti's Party 7:30pm"}];  
+                           localStorage.setItem("TodoLists", JSON.stringify($scope.TodoList)); 
+        }
+        else{
+            $scope.TodoList=TodoLists;
+        }
+        var Dia;
+        $('.add').on('click',function(){
+            Dia = document.querySelector('#newToDO');
+            Dia.show(); 
+            $('.main').addClass('blur');
+        })
+        $('#close').on('click',function(){
+            var title=document.querySelector('#title').value;
+            var todo=document.querySelector('#todo').value;
+            if(title=="" || todo==""){
+                alert("enter both the values");
+            }
+            else{
+                $('.main').removeClass('blur');
+                  Dia.close();
+                var obj={'Title':title,
+                         'Todo':todo};
+                  $scope.TodoList.push(obj);
+                  localStorage.setItem("TodoLists", JSON.stringify($scope.TodoList));
+                  location.reload();
+            }
+        })
+        $('#close1').on('click',function(){
+    Dia_dis = document.querySelector('#displays');
+    Dia_dis.close();
+    $('.main').removeClass('blur');
+
 })
+        
+        })
+
+
+
+function Display(id){
+    var TodoLists=JSON.parse(localStorage.getItem("TodoLists"));
+            Dia_dis = document.querySelector('#displays');
+            console.log(Dia_dis);
+            Dia_dis.show();
+            $('#para').text(TodoLists[id].Todo); 
+            $('.main').addClass('blur');
+
+        }
+
+
